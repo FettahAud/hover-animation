2c07fe39591f86d74ad1a2fa0767a7fc1d4ae251
fettahaud <fettahaud@gmail.com>
2023-10-08 20:26:34 +0300

    last touches

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const links = document.querySelectorAll('.capabilities__list a')
+const background = document.querySelector('.fill-background')
+const images = document.querySelectorAll('.background__images img')
+
+links.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        const index = link.getAttribute('data-project')
+        images.forEach(image => {
+            image.classList.remove('active')
+        })
+        background.classList.add('hidden')
+        images[index].classList.add('active')
+    })
+    link.addEventListener('mouseleave', () => {
+        background.classList.remove('hidden')
+        images.forEach(image => {
+            image.classList.remove('active')
+        })
+    })
+
+})
